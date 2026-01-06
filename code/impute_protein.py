666f21de284f1777452ac9cb3abff72434a7c09c
reliscu <rebeliscu@gmail.com>
2026-01-06 12:55:01 -0800

    diagnosing poor correlation between ADT and imputed protein

--- code/impute_protein.py
+++ code/impute_protein.py
@@ -1,41 +1,27 @@
 import numpy as np
 
-def simulate_protein_from_RNA(Y, topo, true_t, true_l, phi, random_seed=0):
-    ## phi: Protein params
-    np.random.seed(random_seed)
-    
-    L = len(topo) # No. lineages
-    n = Y.shape[0] // L # No. cells per lineage
-    p = Y.shape[1] # No. genes
-    
-    y0 = Y[0, :, 1] # RNA abundance per gene at state 0
-    ss_rate = phi[:, 0] / phi[:, -1] # Steady-state protein production rate = transl_rate/deg_rate
-    p0 = ss_rate * y0 # Initial protein abundance assuming steady-state
-    
-    # Protein production paramters:
-    transl_rates = phi[:, 0].T
-    deg_rates = phi[:, -1].reshape((1, -1))
-    
-    P = np.zeros((n*L, p))
-    
-    for l in range(L):
-        t_l = true_t[true_l == l] # Time points/cells in lineage l     
-        dt = np.diff(t_l, prepend=t_l[0]) # Time step size for each cell along the trajectory
-        t_l = t_l.reshape((-1, 1)) 
-        y_l = Y[l*n:(l+1)*n, :, 1] # Spliced RNAs for lineage l
-        
-        p_l = p0 * np.exp(-deg_rates * t_l) # Pre-existing protein that has not yet degraded
+def impute_protein(X_bw, Q, t, deg_rate, transl_rate=1):
+    Q_max_idx = np.argmax(Q, axis=1) # Each cell's time along the trajectory
 
-        t_diff = t_l - t_l.T # Rows = target time; columns = past times; e.g. t_diff[m, i] = time difference between t_m and t_i 
-        decay_matrix = np.exp(-t_diff[:, :, None] * deg_rates) # Decay_matrix[m, i, p] = decay factor for protein abundance at t_m from RNA available at t_i for gene p
-        mask = (t_diff >= 0)[:, :, None]
-        mask = np.broadcast_to(mask, decay_matrix.shape)
-        decay_matrix = np.where(mask, decay_matrix, 0) # Protein abundance at time t_m can't come from RNA at time t_i > t_m
-        
-        y_l_dt = y_l * dt[:, None] # Multiply each timepoint's RNA by its corresponding time step size (Riemann approximation)
-        # Note to self: protein produced from RNA from time t = m can only be produced during the interval it was measured over, hence: scale RNA contribution at that time step by its time step size
-        protein_contrib = (decay_matrix * y_l_dt[None, :, :]).sum(axis=1) # Integrate RNA counts still surviving up to each time point
-        # Note to self: protein_contrib[target_idx, gene_idx] = np.sum(decay_matrix[target_time, :(target_time + 1), gene_idx] * y_l_dt[None, :(target_time + 1), gene_idx])
-        P[l*n:(l+1)*n] = p_l + transl_rates * protein_contrib # Protein abundance in each cell = pre-existing protein + newly synthesized protein
-            
+    y0 = X_bw[:, 0] # Steady-state RNA abundance per gene
+    ss_rate = transl_rate / deg_rate # Steady-state protein production rate 
+    p0 = ss_rate * y0 # Steady-state protein abundance
+    p_old = p0 * np.exp(-deg_rate * t[Q_max_idx]) # Pre-existing protein that has not yet degraded at the time each cell was observed
+
+    t_reshape = t.reshape((-1, 1))
+    t_diff = t_reshape.T - t_reshape
+    t_diff = t_diff[:, Q_max_idx] # Each column corresponds to a cell: contains time steps with cumulative duration of time leading up to its observed time
+
+    decay_matrix = np.exp(-t_diff * deg_rate) # Decay_matrix[i, m] = decay factor for protein made from RNA available at t_i in cell m
+    mask = (t_diff >= 0) # Protein abundance at time t_m can't come from RNA at time t_i > t_m (cell's observed time)
+    mask = np.broadcast_to(mask, decay_matrix.shape)
+    decay_matrix = np.where(mask, decay_matrix, 0) 
+    
+    dt = np.diff(t, prepend=-t[1])
+    X_bw_dt = X_bw * dt # Multiply each timepoint's RNA by its corresponding time step size (Riemann approximation) 
+    X_bw_dt[X_bw_dt < 0] = 0 # X_bw was populated with '-1' for time points after the cell was observed
+
+    p_new = (decay_matrix * X_bw_dt.T).sum(axis=0) # Integrate RNA counts still surviving up to each time point (until the cell was observed)
+    P = p_old + p_new # Protein abundance in each cell = pre-existing protein + newly synthesized protein
+           
     return P
